cc1059bb91bfa23453574ecdd5a0a143f3587a3a
ninaburg <nina.burg@hotmail.com>
2024-10-30 15:48:10 +0100

    refactor .env

--- flex_container_orchestrator/__init__.py
+++ flex_container_orchestrator/__init__.py
@@ -1,9 +1,12 @@
 """ Initializations """
 
 import os
+from dotenv import load_dotenv
 
 from flex_container_orchestrator.config import logger
 from flex_container_orchestrator.config.service_settings import ServiceSettings
+
+load_dotenv(os.path.join(os.path.dirname(__file__), 'config/.env')) # Load .env variables
 
 # mypy: ignore-errors
 CONFIG = ServiceSettings(

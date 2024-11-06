66c9cdb2a716e3c895e6da62168b5a06b6123bfa
ninaburg <nina.burg@hotmail.com>
2024-11-06 22:08:02 +0000

    comments: add typing in the parsed args

--- flex_container_orchestrator/main.py
+++ flex_container_orchestrator/main.py
@@ -8,10 +8,33 @@ logger = logging.getLogger(__name__)
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--date", required=True, help="Date parameter")
-    parser.add_argument("--location", required=True, help="Location parameter")
-    parser.add_argument("--time", required=True, help="Time parameter")
-    parser.add_argument("--step", required=True, help="Step parameter")
+    parser.add_argument(
+        "--date",
+        type=str,
+        required=True,
+        help="Date parameter in format YYYYMMDD"
+    )
+
+    parser.add_argument(
+        "--location",
+        type=str,
+        required=True,
+        help="Location parameter"
+    )
+
+    parser.add_argument(
+        "--time",
+        type=str,
+        required=True,
+        help="Time parameter in format HH"
+    )
+
+    parser.add_argument(
+        "--step",
+        type=str,
+        required=True,
+        help="Step parameter"
+    )
     args = parser.parse_args()
 
     flexpart_service.launch_containers(args.date, args.location, args.time, args.step)
